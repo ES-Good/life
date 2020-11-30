efd1828085afc2b12c8a85588df7b5c9de884db6
ES-Good <46601455+ES-Good@users.noreply.github.com>
2020-12-01 02:51:53 +0400

    't'

---
+++ js/common.js
@@ -0,0 +1,19 @@
+$('.gallery-reviews').slick({
+    dots: true,
+    dotsClass: 'slick-dots dots-reviews',
+    prevArrow: '<button class="slick-prev slick-arrow prev-reviews" aria-label="Previous" type="button" style="">Previous</button>',
+    nextArrow: '<button class="slick-next slick-arrow next-reviews" aria-label="Next" type="button" style="">Next</button>',
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+  });
+
+  $('.certificates').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
