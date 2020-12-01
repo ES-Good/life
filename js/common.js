c17f1fe6706c830a24d0e7cca69a2131a56e5ae6
Максим Казачков <evgenshtanko@ya.ru>
2020-12-01 17:07:27 +0400

    't'

--- js/common.js
+++ js/common.js
@@ -19,3 +19,12 @@ $('.gallery-reviews').slick({
     centerMode: true,
     variableWidth: true
   });
+  
+  $('.mobile-staff').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
